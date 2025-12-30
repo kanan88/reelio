fe4671d0a7e17d9f3f2643ff3a5a2784e5987ffd
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-12-30 22:16:47 +0100

    implement home page fetching real live data from api

---
+++ components/MovieCard.tsx
@@ -0,0 +1,51 @@
+import { Link } from 'expo-router'
+import React from 'react'
+import { Image, Text, TouchableOpacity, View } from 'react-native'
+
+import { icons } from '@/constants/icons'
+
+const MovieCard = ({
+  id,
+  poster_path,
+  title,
+  vote_average,
+  release_date
+}: Movie) => {
+  return (
+    <Link href={`/movies/${id}`} asChild>
+      <TouchableOpacity className="w-[30%]">
+        <Image
+          source={{
+            uri: poster_path
+              ? `https://image.tmdb.org/t/p/w500${poster_path}`
+              : 'https://placehold.co/600x400/1a1a1a/ffffff.png'
+          }}
+          className="w-full h-52 rounded-lg"
+          resizeMode="cover"
+        />
+
+        <Text className="text-white text-sm font-bold mt-2" numberOfLines={1}>
+          {title}
+        </Text>
+
+        <View className="flex-row items-center justify-start gap-x-1">
+          <Image source={icons.star} className="size-4" />
+          <Text className="text-white text-xs font-bold uppercase">
+            {Math.round(vote_average * 10) / 10}
+          </Text>
+        </View>
+
+        <View className="flex-row items-center justify-between mt-2">
+          <Text className="text-gray-500 text-xs font-medium mt-1">
+            {release_date.split('-')[0]}
+          </Text>
+          <Text className="text-gray-500 text-xs font-medium uppercase">
+            Movie
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </Link>
+  )
+}
+
+export default MovieCard
